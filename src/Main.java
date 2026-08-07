fc5a7a772f2933898d366366a3714910787339e0
albaraa-prog <albraa9021@gmail.com>
2026-08-08 01:07:28 +0400

    first

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Brick Breaker");
+
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLayout(null);
+
+    }
+}
